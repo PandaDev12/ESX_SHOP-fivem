00315277cf108ac2154192bf29eae1e642c3f16c
PandaDev12 <112827496+PandaDev12@users.noreply.github.com>
2022-09-04 22:23:53 +0200

    Delete shop directory

--- shop/sql/shop.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO items (name,label,weight,rare,can_remove) VALUES ("vodka","Vodka",1,0,1);
-INSERT INTO items (name,label,weight,rare,can_remove) VALUES ("oasis","Oasis",1,0,1);
-INSERT INTO items (name,label,weight,rare,can_remove) VALUES ("pizza","Pizza",1,0,1);
-INSERT INTO items (name,label,weight,rare,can_remove) VALUES ("kinder","Kinder",1,0,1);
-INSERT INTO items (name,label,weight,rare,can_remove) VALUES ("twix","Twix",1,0,1)
